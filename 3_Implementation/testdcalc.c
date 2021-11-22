6480fba3e68fe772db7a1ac6c0ec2663d873787b
Ajish Saji <80235704+legends07@users.noreply.github.com>
2021-11-22 08:45:27 +0530

    Multi File

---
+++ 3_Implementation/testdcalc.c
@@ -0,0 +1,19 @@
+#include "unity.h"
+#include "Dcalc.h"
+void setUp()
+{
+}
+void tearDown()
+{
+}
+int main(void)
+{
+  UNITY_BEGIN();
+
+  RUN_TEST();
+  RUN_TEST();
+  RUN_TEST();
+  RUN_TEST();
+  
+  return UNITY_END();
+}
