c2b7646c36b30305b5ee26f0f79028b3732c880a
Ajish Saji <80235704+legends07@users.noreply.github.com>
2021-11-22 15:48:09 +0530

    Update test_dcalc.c

--- 3_Implementation/test/test_dcalc.c
+++ 3_Implementation/test/test_dcalc.c
@@ -1,1 +1,19 @@
+#include "unity.h"
+#include "dcalc.h"
+void setUp()
+{
+}
+void tearDown()
+{
+}
+int main(void)
+{
+  UNITY_BEGIN();
 
+  RUN_TEST();
+  RUN_TEST();
+  RUN_TEST();
+  RUN_TEST();
+  
+  return UNITY_END();
+}
