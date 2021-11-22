a3f98c4e47aa1675a0260c5d8ed4a688bb05866f
Ajish Saji <80235704+legends07@users.noreply.github.com>
2021-11-22 15:46:29 +0530

    Create dcalc.h

---
+++ 3_Implementation/src/dcalc.h
@@ -0,0 +1,15 @@
+/**
+ * @file calc.c
+ * @author Ajish Saji (ajish.saji@ltts.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "stdlib.h"
+#include <stdlib.h>
+#include "unity.h"
+#include "dcalc.h"
+#include <math.h>
